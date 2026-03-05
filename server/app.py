e564afdeaa5bba19c373c5fd64cdc30fa5935204
Naziyatabassum <naziyaabdul12@gmail.com>
2026-03-05 09:37:37 +0530

    fix cors headers

--- server/app.py
+++ server/app.py
@@ -23,7 +23,14 @@ load_dotenv()
 app = Flask(__name__)
 
 # ------------------ CORS ------------------
-CORS(app, origins=["https://farming-hmppgzyml-naziyatabassums-projects.vercel.app"])
+CORS(app)
+
+@app.after_request
+def add_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
+    return response
 # ------------------ CONFIG ------------------
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
